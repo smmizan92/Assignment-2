42f1723a62760f62fa87b5191774e93e2364378f
smmizan92 <mizantmkm@gmail.com>
2026-09-17 21:26:59 +0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Movies from "./pages/Movie";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
